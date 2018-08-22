f3da2dadf4063860c13b7ab6cccbd52644b44503
Trahotonic <romcheg95@gmail.com>
2018-08-22 15:30:03 +0300

    norme live_ld...

--- src/live_ld_st_add_sub.c
+++ src/live_ld_st_add_sub.c
@@ -4,7 +4,7 @@
 
 #include "../inc/corewar.h"
 
-void  live(t_process *processor,int i, t_player *pl)
+void  live(t_process *pro,int i, t_player *pl)
 {
 	char *tmp;
 	t_player *tmppl;
@@ -12,12 +12,12 @@ void  live(t_process *processor,int i, t_player *pl)
 	tmppl = pl;
 	while (tmppl)
 	{
-		if (tmppl->playerNumber == processor->pl_num)
+		if (tmppl->playerNumber == pro->pl_num)
 			tmppl->liveCount++;
 		tmppl = tmppl->next;
 	}
 	tmp = ft_uitoa_base2(pl->playerNumber, 16);
-	while(pl && !ft_strequ(tmp, processor->arg1))
+	while(pl && !ft_strequ(tmp, pro->arg1))
 	{
 		ft_strdel(&tmp);
 		pl = pl->next;
@@ -25,133 +25,136 @@ void  live(t_process *processor,int i, t_player *pl)
 			tmp = ft_uitoa_base2(pl->playerNumber, 16);
 	}
 	ft_strdel(&tmp);
-	processor->alive = 1;
+	pro->alive = 1;
 	if (pl)
 		pl->lastAlive = i;
-	processor->cur_pos = (processor->iterator + processor->cur_pos) % 8192;
-	processor->iterator = 0;
+	pro->cur_pos = (pro->iterator + pro->cur_pos) % 8192;
+	pro->iterator = 0;
 }
 
-void		ld(t_process *processor, unsigned char *map)
+void ld_sup(t_process *pro, unsigned char* map)
 {
-	int		n;
-	char	*arg1;
-	int		k;
-
-	if (ft_strlen(processor->arg3) == 2)
-		processor->iterator -= 2;
-	if (ft_strlen(processor->arg3) == 4)
-		processor->iterator -= 4;
-	if (ft_strlen(processor->arg3) == 8)
-		processor->iterator -= 8;
-	if (ft_strlen(processor->arg1) == 2 || ft_strlen(processor->arg1) == 0 || ft_strlen(processor->arg2) != 2 || processor->iC == 1)
+	int  k;
+	int  n;
+	char *arg1;
+	k = ((((short)ft_atoi_base(pro->arg1, 16)) % IDX_MOD)
+		 * 2 + pro->cur_pos) % (MEM_SIZE * 2);
+	n = 0;
+	arg1 = ft_strnew(8);
+	while (n < 8)
 	{
-		processor->cur_pos = (processor->iterator + processor->cur_pos) % (MEM_SIZE * 2);
-		processor->iterator = 0;
+		k %= (MEM_SIZE * 2);
+		arg1[n++] = map[k++];
+	}
+	pro->reg[ft_atoi_base(pro->arg2, 16) - 1] = ft_atoi_base(arg1, 16);
+}
+
+void  ld(t_process *pro, unsigned char *map)
+{
+	if (ft_strlen(pro->arg3) == 2)
+		pro->iterator -= 2;
+	if (ft_strlen(pro->arg3) == 4)
+		pro->iterator -= 4;
+	if (ft_strlen(pro->arg3) == 8)
+		pro->iterator -= 8;
+	if (ft_strlen(pro->arg1) == 2 || ft_strlen(pro->arg1) == 0
+		|| ft_strlen(pro->arg2) != 2 || pro->iC == 1)
+	{
+		pro->cur_pos = (pro->iterator + pro->cur_pos) % (MEM_SIZE * 2);
+		pro->iterator = 0;
 		return ;
 	}
-	if (ft_strlen(processor->arg1) == 8)
-	{
-		processor->reg[ft_atoi_base(processor->arg2, 16) - 1] =
-				(unsigned int)ft_atoi_base(processor->arg1, 16);
-	}
-	else if (ft_strlen(processor->arg1) == 4)
-	{
-		k = ((((short)ft_atoi_base(processor->arg1, 16)) % IDX_MOD) * 2 + processor->cur_pos) % (MEM_SIZE * 2);
-		n = 0;
-		arg1 = ft_strnew(8);
-		while (n < 8)
-		{
-			k %= (MEM_SIZE * 2);
-			arg1[n++] = map[k++];
-		}
-		processor->reg[ft_atoi_base(processor->arg2, 16) - 1] = ft_atoi_base(arg1, 16);
-	}
-	if (processor->reg[ft_atoi_base(processor->arg2, 16) - 1] == 0)
-		processor->carry = 1;
+	if (ft_strlen(pro->arg1) == 8)
+		pro->reg[ft_atoi_base(pro->arg2, 16) - 1] =
+				(unsigned int)ft_atoi_base(pro->arg1, 16);
+	else if (ft_strlen(pro->arg1) == 4)
+		ld_sup(pro, map);
+	if (pro->reg[ft_atoi_base(pro->arg2, 16) - 1] == 0)
+		pro->carry = 1;
 	else
-		processor->carry = 0;
-	processor->cur_pos = (processor->iterator + processor->cur_pos) % 8192;
-	processor->iterator = 0;
+		pro->carry = 0;
+	pro->cur_pos = (pro->iterator + pro->cur_pos) % 8192;
+	pro->iterator = 0;
 }
 
-void	st(t_process *processor, unsigned char *map, t_viz_data *viz_data)
+void st_sup(t_process *pro, unsigned char *map, t_viz_data *vizData)
 {
-	int		n;
-	char	*tmp;
-	int		k;
+	int    n;
+	int    k;
+	char   *tmp;
 
-	if (ft_strlen(processor->arg3) == 2)
-		processor->iterator -= 2;
-	if (ft_strlen(processor->arg3) == 4)
-		processor->iterator -= 4;
-	if (ft_strlen(processor->arg3) == 8)
-		processor->iterator -= 8;
-	if (ft_strlen(processor->arg1) != 2 || ft_strlen(processor->arg2) == 8 || ft_strlen(processor->arg2) == 0 || processor->iC == 1)
+	k = ((((short)ft_atoi_base(pro->arg2, 16) ) % IDX_MOD)
+		 * 2 + pro->cur_pos) % (MEM_SIZE * 2);
+	n = 0;
+	tmp = ft_itoa_base(pro->reg[ft_atoi_base(pro->arg1, 16) - 1], 16);
+	convert(&tmp);
+	if (k < 0)
+		k = (MEM_SIZE * 2 + k) % (MEM_SIZE * 2);
+	while (n < 8)
 	{
-		processor->cur_pos = (processor->iterator + processor->cur_pos) % (MEM_SIZE * 2);
-		processor->iterator = 0;
-		return ;
+		k %= (MEM_SIZE * 2);
+		vizData->mark_timeout[k] = 100;
+		vizData->viz_data[k] = pro->pl_number;
+		map[k++] = tmp[n++];
 	}
-	if (ft_strlen(processor->arg2) == 4)
-	{
-		k = ((((short)ft_atoi_base(processor->arg2, 16) ) % IDX_MOD) * 2 + processor->cur_pos) % (MEM_SIZE * 2);
-		n = 0;
-		tmp = ft_itoa_base(processor->reg[ft_atoi_base(processor->arg1, 16) - 1], 16);
-		convert(&tmp);
-		if (k < 0)
-		{
-			k = (MEM_SIZE * 2 + k) % (MEM_SIZE * 2);
-		}
-		while (n < 8)
-		{
-			k %= (MEM_SIZE * 2);
-			viz_data->mark_timeout[k] = 100;
-			viz_data->viz_data[k] = processor->pl_number;
-			map[k++] = tmp[n++];
-		}
-	}
-	else if (ft_strlen(processor->arg2) == 2)
-		processor->reg[ft_atoi_base(processor->arg2, 16) - 1] =
-				processor->reg[ft_atoi_base(processor->arg1, 16) - 1];
-	processor->cur_pos = (processor->iterator + processor->cur_pos) % 8192;
-	processor->iterator = 0;
 }
 
-void		add(t_process *processor)
+void st(t_process *pro, unsigned char *map, t_viz_data *vizData)
 {
-	if (ft_strlen(processor->arg1) != 2 || ft_strlen(processor->arg2) != 2 || ft_strlen(processor->arg3) != 2 || processor->iC == 1)
+	if (ft_strlen(pro->arg3) == 2)
+		pro->iterator -= 2;
+	if (ft_strlen(pro->arg3) == 4)
+		pro->iterator -= 4;
+	if (ft_strlen(pro->arg3) == 8)
+		pro->iterator -= 8;
+	if (ft_strlen(pro->arg1) != 2 || ft_strlen(pro->arg2) == 8
+		|| ft_strlen(pro->arg2) == 0 || pro->iC == 1)
 	{
-		processor->cur_pos = (processor->iterator + processor->cur_pos) % (MEM_SIZE * 2);
-		processor->iterator = 0;
+		pro->cur_pos = (pro->iterator + pro->cur_pos) % (MEM_SIZE * 2);
+		pro->iterator = 0;
 		return ;
 	}
-	processor->reg[ft_atoi_base(processor->arg3, 16) - 1] =
-			processor->reg[ft_atoi_base(processor->arg1, 16) - 1] +
-			processor->reg[ft_atoi_base(processor->arg2, 16) - 1];
-	if (processor->reg[ft_atoi_base(processor->arg3, 16) - 1] == 0)
-		processor->carry = 1;
+	if (ft_strlen(pro->arg2) == 4)
+		st_sup(pro, map, vizData);
+	else if (ft_strlen(pro->arg2) == 2)
+		pro->reg[ft_atoi_base(pro->arg2, 16) - 1] =
+				pro->reg[ft_atoi_base(pro->arg1, 16) - 1];
+	pro->cur_pos = (pro->iterator + pro->cur_pos) % 8192;
+	pro->iterator = 0;
+}
+
+void  add(t_process *pro)
+{
+	pro->cur_pos = (pro->iterator + pro->cur_pos) % (MEM_SIZE * 2);
+	pro->iterator = 0;
+	if (ft_strlen(pro->arg1) != 2 || ft_strlen(pro->arg2) != 2
+		|| ft_strlen(pro->arg3) != 2 || pro->iC == 1)
+		return ;
+	pro->reg[ft_atoi_base(pro->arg3, 16) - 1] =
+			pro->reg[ft_atoi_base(pro->arg1, 16) - 1] +
+			pro->reg[ft_atoi_base(pro->arg2, 16) - 1];
+	if (pro->reg[ft_atoi_base(pro->arg3, 16) - 1] == 0)
+		pro->carry = 1;
 	else
-		processor->carry = 0;
-	processor->cur_pos = (processor->iterator + processor->cur_pos) % 8192;
-	processor->iterator = 0;
+		pro->carry = 0;
+	pro->cur_pos = (pro->iterator + pro->cur_pos) % 8192;
+	pro->iterator = 0;
 }
 
-void		sub(t_process *processor)
+void  sub(t_process *pro)
 {
-	if (ft_strlen(processor->arg1) != 2 || ft_strlen(processor->arg2) != 2 || ft_strlen(processor->arg3) != 2 || processor->iC == 1)
-	{
-		processor->cur_pos = (processor->iterator + processor->cur_pos)  % (MEM_SIZE * 2);
-		processor->iterator = 0;
+	pro->cur_pos = (pro->iterator + pro->cur_pos)  % (MEM_SIZE * 2);
+	pro->iterator = 0;
+	if (ft_strlen(pro->arg1) != 2 || ft_strlen(pro->arg2) != 2
+		|| ft_strlen(pro->arg3) != 2 || pro->iC == 1)
 		return ;
-	}
-	processor->reg[ft_atoi_base(processor->arg3, 16) - 1] =
-			processor->reg[ft_atoi_base(processor->arg1, 16) - 1] -
-			processor->reg[ft_atoi_base(processor->arg2, 16) - 1];
-	if (processor->reg[ft_atoi_base(processor->arg3, 16) - 1] == 0)
-		processor->carry = 1;
+	pro->reg[ft_atoi_base(pro->arg3, 16) - 1] =
+			pro->reg[ft_atoi_base(pro->arg1, 16) - 1] -
+			pro->reg[ft_atoi_base(pro->arg2, 16) - 1];
+	if (pro->reg[ft_atoi_base(pro->arg3, 16) - 1] == 0)
+		pro->carry = 1;
 	else
-		processor->carry = 0;
-	processor->cur_pos = (processor->iterator + processor->cur_pos) % 8192;
-	processor->iterator = 0;
+		pro->carry = 0;
+	pro->cur_pos = (pro->iterator + pro->cur_pos) % 8192;
+	pro->iterator = 0;
 }
